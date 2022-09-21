d5a6d89cddfd531789b7dd1c426e91d73ec74917
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2022-09-21 21:18:15 +0530

    Better version

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 let container=document.querySelector('.container')
-const squares=3000;
+const squares=10000;
+
 const color=["#056CF2","#05AFF2","#F2E205","#F28705","#A62103"]
+let ok=false;
+let c="#1d1d1d";
+
 
 for(let i=0;i<squares;i++){
     let ele=document.createElement('span')
     ele.classList.add('sq')
-
     ele.addEventListener('mouseover',()=>{
-        const c=color[Math.floor(Math.random() * color.length)]
+        onHold()
         ele.style.background=c;
         ele.style.boxShadow=`0 0 2px ${c},0 0 10px ${c}`;
     });
 
-    ele.addEventListener('mouseout',()=>{
-        ele.style.background="#1d1d1d";
-        ele.style.boxShadow="0 0 2px #000"
-    });0
     container.appendChild(ele)
 }
+
+function onHold(){
+    if(ok==true){
+        c=c=color[Math.floor(Math.random() * color.length)]
+    }
+    else{
+        c="#1d1d1d"
+    }
+}
+
+container.addEventListener('mousedown',()=>{
+    ok=true
+})
+
+container.addEventListener('mouseup',()=>{
+    ok=false
+})
+
+
+
+
+
