528a46b4bf7652b7f3da2e2dc5d2ea0a68cbeb0b
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2022-10-13 08:40:38 +0530

    secondCommit

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 let container=document.querySelector('.container')
-const squares=10000;
+const squares=6900;
 
 const color=["#056CF2","#05AFF2","#F2E205","#F28705","#A62103"]
 let ok=false;
-let c="#1d1d1d";
+let notOk=false;
+let c;
+let turn;
 
 
 for(let i=0;i<squares;i++){
     let ele=document.createElement('span')
     ele.classList.add('sq')
     ele.addEventListener('mouseover',()=>{
-        onHold()
-        ele.style.background=c;
-        ele.style.boxShadow=`0 0 2px ${c},0 0 10px ${c}`;
+        onHold(ele)
     });
 
     container.appendChild(ele)
 }
 
-function onHold(){
+function onHold(ele){
     if(ok==true){
-        c=c=color[Math.floor(Math.random() * color.length)]
+        c=document.querySelector('#colorpicker').value
+        ele.style.background=c;
+        ele.style.boxShadow=`0 0 2px ${c},0 0 10px ${c}`;
     }
-    else{
-        c="#1d1d1d"
+    if(notOk==true && ok==false){
+        ele.style.background="#1d1d1d";
+        ele.style.boxShadow= "none";
+
     }
+
 }
+
+
+document.querySelector('.erase').addEventListener('click',()=>{
+    document.querySelector('.erase').classList.toggle('erase-selected')
+    turn="eraser"
+    if(!document.querySelector('.erase').classList.contains('erase-selected')){
+        turn="draw"
+    }
+})
 
 container.addEventListener('mousedown',()=>{
     ok=true
+    if(turn=="eraser"){
+        ok=false
+        notOk=true
+    }
 })
 
 container.addEventListener('mouseup',()=>{
     ok=false
+    if(turn=="eraser"){
+        notOk=false
+    }
 })
 
-
-
-
-
+document.querySelector('.reset').addEventListener('click',()=>location.reload())
